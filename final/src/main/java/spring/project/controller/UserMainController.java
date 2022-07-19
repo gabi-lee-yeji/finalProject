889661177c13116abc0f15827eac3d034cf42ac7
gabiLee <leegabi427@gmail.com>
2022-07-19 09:58:12 +0900

    0718 자격증 일정 등록 완

--- final/src/main/java/spring/project/controller/UserMainController.java
+++ final/src/main/java/spring/project/controller/UserMainController.java
@@ -1,5 +1,7 @@
 package spring.project.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,12 +20,14 @@ public class UserMainController {
 	private AdminService adminService;
 	
 	@RequestMapping("")
-	public String userMain(Model model) {
+	public String userMain(HttpSession session, Model model) {
 		//회원등급 자동조정
 		adminService.updateMemberStatus();
 		
-		//사용자 맞춤 인기자격증 Top 10
-		model.addAttribute("clientList", service.getClientTopCerti(null));
+		//사용자 맞춤 인기자격증 Top 10 
+		//String memid = (String) session.getAttribute("sid");
+		//model.addAttribute("clientList", service.getClientTopCerti(memid));
+		//model.addAttribute("clientList", service.getClientTopCerti("test"));
 		return "/main";
 	}
 	
